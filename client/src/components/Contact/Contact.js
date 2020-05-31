2edc4a906219fee93fd1931fc2121d3e0c436517
Dylan Ross Ludwick <dyludwick@gmail.com>
2020-05-30 20:04:37 -0600

    hook up remaining inputs to state

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -4,8 +4,11 @@ import phoneMask from './utils';
 import styles from './Contact.css';
 
 function Contact() {
+  const { changeHandler: handleMessageTypeChange, value: messageType } = useInput('');
   const { changeHandler: handleNameChange, value: name } = useInput('');
+  const { changeHandler: handleEmailChange, value: email } = useInput('');
   const { changeHandler: handlePhoneChange, value: phone } = useInput('');
+  const { changeHandler: handleMessageChange, value: message } = useInput('');
 
   return (
     <div className={styles.Contact}>
@@ -13,10 +16,16 @@ function Contact() {
       <form>
         <label htmlFor="message-type">
           Message Type:
-          <select className={styles.formInput} name="message-type" required>
+          <select
+            className={styles.formInput}
+            name="message-type"
+            onChange={(e) => handleMessageTypeChange(e.target.value)}
+            required
+            value={messageType}
+          >
             <option value="">Please Select a Message Type</option>
-            <option value="collaboration">Collaboration Inquiry</option>
-            <option value="bug">Bug Report</option>
+            <option value="collaboration inquiry">Collaboration Inquiry</option>
+            <option value="bug report">Bug Report</option>
             <option value="question">Question</option>
             <option value="other">Other</option>
           </select>
@@ -39,8 +48,10 @@ function Contact() {
             className={styles.formInput}
             name="email"
             required
+            onChange={(e) => handleEmailChange(e.target.value)}
             placeholder="name@domain.com"
             type="email"
+            value={email}
           />
         </label>
         <label htmlFor="phone">
@@ -62,9 +73,11 @@ function Contact() {
           <textarea
             className={styles.formInput}
             name="message"
+            onChange={(e) => handleMessageChange(e.target.value)}
             placeholder="Ground Control to Major Tom..."
             required
             rows="5"
+            value={message}
           />
         </label>
         <button type="submit">send</button>
